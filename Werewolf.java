d3da7de0c70211a164aa6a724a3c793ad85c7604
dimysiklapo4ka <dimysiklapo4ka@gmail.com>
2017-04-02 02:47:17 +0300

    Мне не нравится переделать в дальнейшемÐ

--- Werewolf.java
+++ Werewolf.java
@@ -1,7 +1,25 @@
 package InitDnepr.werewolf;
 
+import java.util.Scanner;
+
+
 /**
  * Created by d1mys1klapo4ka on 01.04.2017.
  */
 public class Werewolf {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    String text = scanner.nextLine();
+    scanner.close();
+    String[] werewolf = text.split(" ");
+
+        for (int i = 0, n = werewolf.length; i < n; i++) {
+            StringBuffer abc = new StringBuffer(werewolf[i]);
+            abc.reverse();
+            System.out.println(abc);
+        }
+    }
 }
